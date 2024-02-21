00ba2ac7d1ddf7c576dec20604cac1b01b60743c
Gabriella Guimaraes <gabigara13@gmail.com>
2024-02-21 12:11:46 -0300

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import { NavbarComponent } from './components/Navbar';
 
 function App() {
   return (
     <div>
+      <NavbarComponent></NavbarComponent>
       <h1>Hello there!</h1>
     </div>
   );
