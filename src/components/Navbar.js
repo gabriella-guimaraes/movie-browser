c5fce451208927910b21b624260fbce7a27ba25b
Gabriella Guimaraes <gabigara13@gmail.com>
2024-02-23 21:59:08 -0300

    fix: search component redirection to search page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export function NavbarComponent({searchText, setSearchText}) {
+  const navigate = useNavigate();
   const updateSearchText = (e) => {
+    navigate('/search');
     setSearchText(e.target.value);
   };
 
--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -2,7 +2,6 @@ import { HeroComponent } from "./Hero";
 
 export function SearchViewComponent ({keyword, searchResults}) {
     const tittle = `You are searching for ${keyword}`
-    console.log(searchResults, "are the search results");
     return (
         <div>
             <HeroComponent text={tittle} />
