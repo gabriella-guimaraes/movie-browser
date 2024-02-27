02449a5a04abf5f811131b37225f331c435b3da5
Gabriella Guimaraes <gabigara13@gmail.com>
2024-02-27 20:44:08 -0300

    fix: search message for no results bug

--- src/pages/SearchView.js
+++ src/pages/SearchView.js
@@ -2,21 +2,23 @@ import { HeroComponent } from "../components/Hero";
 import { MovieCardComponent } from "../components/MovieCard";
 
 export function SearchViewComponent({ keyword, searchResults }) {
-  const tittle = `You are searching for ${keyword}`;
+  const title = `You are searching for ${keyword}`;
   const movieResults = searchResults?.map((obj, i) => {
     return <MovieCardComponent movie={obj} key={i} />;
   });
   return (
     <div>
-      <HeroComponent text={tittle} />
-      {movieResults && (
-        <div className="container">
-          <div className="row">{movieResults}</div>
-        </div>
-      )}{" "}
+    <HeroComponent text={title} />
+    {searchResults && searchResults.length > 0 && (
+      <div className="container">
+        <div className="row">{movieResults}</div>
+      </div>
+    )}
+    {searchResults && searchResults.length === 0 && (
       <h2 className="p-5">
         {"Sorry, we couldn't find anything for" + keyword}
       </h2>
-    </div>
+    )}
+  </div>
   );
 }
