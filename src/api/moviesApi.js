b038ec4fd99508c2921df6175d888be0b05fcd85
gabriella-guimaraes <gabigara13@gmail.com>
2025-07-18 18:14:27 -0300

    fix: integrate movie fetching functions and add upcoming movies feature

--- src/api/moviesApi.js
+++ src/api/moviesApi.js
@@ -1,74 +1,86 @@
 const url = "https://api.themoviedb.org/3";
+const token = process.env.REACT_APP_API_KEY;
 
 const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwY2JmZDQ2MTc0NjI4NTA3NjJiYTA0NTlkMWVkMjY2ZiIsIm5iZiI6MTcwODczMzcwMi42ODYsInN1YiI6IjY1ZDkzNTA2YjA0NjA1MDE0OTM0NTJiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1bnesA0MEgiQiDy5wirdYN8fQ5Z3J2ztqOzFYgMaUVI'
-      }
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${token}`,
+  },
 };
 
-export async function fetchMovieDetails(id) {
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwY2JmZDQ2MTc0NjI4NTA3NjJiYTA0NTlkMWVkMjY2ZiIsIm5iZiI6MTcwODczMzcwMi42ODYsInN1YiI6IjY1ZDkzNTA2YjA0NjA1MDE0OTM0NTJiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1bnesA0MEgiQiDy5wirdYN8fQ5Z3J2ztqOzFYgMaUVI'
-    }
-  };
+export async function fetchMoviesByKeyword(searchText) {
   try {
-    const response = await fetch(`${url}/movie/${id}`, options);
-    if (!response.ok) {
-      throw new Error("Erro ao obter detalhes do filme");
-    }
+    const response = await fetch(`${url}/search/movie?language=en-US&query=${searchText}&page=1&include_adult=false`, options);
     const data = await response.json();
-    return data;
+
+    return data.results;
+
   } catch (error) {
-    console.error("Ocorreu um erro:", error);
+    console.log("Error ao obter filmes:", error);
+    throw error;
+  }
+}
+
+export async function fetchMovieDetails(id) {
+  try {
+    const response = await fetch(`${url}/movie/${id}?language=en-US`, options);
+    const data = await response.json();
+
+    return data;
+
+  } catch (error) {
+    console.log("Error ao obter filmes:", error);
     throw error;
   }
 }
 
 export async function fetchCredits(id) {
-    try {
-      const response = await fetch(
-        `${url}/movie/${id}/credits?language=en-US`, options
-      );
-      if (!response.ok) {
-        throw new Error("Erro ao obter créditos do filme");
-      }
-      const data = await response.json();
-      return data.cast; // Retorna apenas a parte do elenco dos dados recebidos
-    } catch (error) {
-      console.error("Ocorreu um erro ao obter créditos:", error);
-      throw error;
-    }
-  }
+  try {
+    const response = await fetch(`${url}/movie/${id}/credits?language=en-US`, options);
+    const data = await response.json();
+    
+    return data.cast; // Retorna apenas a parte do elenco dos dados recebidos
 
-  export async function fetchRecommendations(id) {
-    try {
-      const response = await fetch(
-        `${url}/movie/${id}/recommendations?language=en-US&page=1`, options
-      );
-      if(!response.ok) {
-        throw new Error("Ocorreu um erro ao obter recomendações");
-      }
-      const data = await response.json();
-      return data.results;
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
+  } catch (error) {
+    console.error("Ocorreu um erro ao obter créditos:", error);
+    throw error;
   }
+}
 
-  export async function fetchPopularMovies(pages = 5) {
-    try {
-      const response = await fetch(`${url}/discover/movie?include_adult=false&include_video=false&language=en-US&page=${pages}&sort_by=popularity.desc`, options);
-      const data = await response.json();
-      return data.results;
+export async function fetchRecommendations(id) {
+  try {
+    const response = await fetch(`${url}/movie/${id}/recommendations?language=en-US&page=1`, options);
+    const data = await response.json();
 
-    } catch (error) {
-      console.log("Error ao obter filmes populares:", error);
-      throw error;
-    }
+    return data.results;
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
+}
+
+export async function fetchPopularMovies(pages = 5) {
+  try {
+    const response = await fetch(`${url}/discover/movie?include_adult=false&include_video=false&language=en-US&page=${pages}&sort_by=popularity.desc`, options);
+    const data = await response.json();
+
+    return data.results;
+  } catch (error) {
+    console.log("Error ao obter filmes populares:", error);
+    throw error;
+  }
+}
+
+export async function fetchUpcomingMovies() {
+  try {
+    const response = await fetch(`${url}/movie/upcoming?language=en-US&page=1`, options);
+    console.log(response);
+    const data = await response.json();
+
+    return data.results;
+  } catch (error) {
+    console.log("Error ao obter filmes futuros:", error);
+    throw error;
+  }
+}
